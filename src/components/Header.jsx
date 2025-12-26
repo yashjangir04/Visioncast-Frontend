03e16650e2a25c6ea6d3070819e5aea3741d5f58
Astha-codes16 <rekharanidk2017@gmail.com>
2025-12-27 03:31:03 +0530

    Navbar and header

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+       <div className='my-10 mx-30 py-0'>
+        <h1 className='text-3xl my-2 sm:text-6xl font-semibold sm:leading-16 text-[#FEFFFF]'>Unlock the world of video for <br/><span className='text-primary'>Everyone</span></h1>
+              {/* <h1 className='text-3xl my-2 sm:text-6xl font-semibold sm:leading-16 text-gray-700'>Your Own <span className='text-primary'>Blogging</span> <br/>Platform.</h1> */}
+              <p className='text-1xl my-2 sm:text-[1.3rem]  sm:leading-16 text-[#FEFFFF]'>Automated AI-powered accessibility.Generate detailed Audio description for any video</p>
+              </div>
+       
+
+    </>
+  )
+}
+
+export default Header
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../components/navbar'
+import Header from '../components/Header'
+
+const Home = () => {
+  return (
+    <>
+    <Navbar/>
+    <Header/>
+    </>
+  )
+}
+
+export default Home
